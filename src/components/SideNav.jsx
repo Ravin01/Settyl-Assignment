8c399d94e0efbfdb41698037bdad6bc276cbb88e
Ravin01 <ravinganapathi@gmail.com>
2024-02-06 23:28:01 +0530

    updated

--- src/components/SideNav.jsx
+++ src/components/SideNav.jsx
@@ -12,24 +12,35 @@ const SideNav = () => {
     setArrowDepartment(isOpenDepartmentDiv ? "right" : "down");
   };
 
-
   return (
     <div className="sideNav-container">
       <div className="sideNav-header">
         <h2>Administration</h2>
       </div>
       <div className="sideNav-body">
-        <Link to="/dashboard" className="sideNav-body-link" >Dashboard</Link>
-        <Link to="/students" className="sideNav-body-link" >Students</Link>
-        <Link to="/program" className="sideNav-body-link" >Program</Link>
+        <Link to="/dashboard" className="sideNav-body-link">
+          <i className="fas fa-fw fa-tachometer-alt"></i>
+          <p>Dashboard</p>
+        </Link>
+
+        <Link to="/students" className="sideNav-body-link">
+          <i className="fa-solid fa-user"></i>
+          <p>Students</p>
+        </Link>
+
+        <Link to="/program" className="sideNav-body-link">
+          <i className="fa-solid fa-calendar"></i>
+          <p>Program</p>
+        </Link>
+
         <div className="sideNav-filter">
-          <h4 onClick={handleOpenDepartmentDiv}>Department</h4>
+          <h3 onClick={handleOpenDepartmentDiv}>Department</h3>
           <i className={`fa-solid fa-angle-${arrowDepartment}`}></i>
           {isOpenDepartmentDiv && (
             <div className="sideNav-items">
               <p>ECE</p>
               <p>EEE</p>
-              <p>CSE</p> 
+              <p>CSE</p>
               <p>Civil</p>
             </div>
           )}
